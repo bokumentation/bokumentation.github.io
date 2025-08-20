b8f9057d9f55a7756da8c9029a14c9ac31df6639
bokumentation <ibraheli41@gmail.com>
2025-08-20 15:38:38 +0700

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -21,7 +21,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   site: 'https://bokumentation.github.io',
-  base: '/my-repo',
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
