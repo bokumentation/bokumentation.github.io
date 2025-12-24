aee37796ec198209c36d3bd8fa2f457105d73908
Ibe Ibrahim <ibraheli41@gmail.com>
2025-12-24 19:32:41 +0700

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -20,7 +20,7 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'https://www.bokumentation.site',
+  site: 'https://bokumentation.github.io',
   outDir: './dist',
   integrations: [
     expressiveCode({
