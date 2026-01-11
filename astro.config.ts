ad0a8855724e296ee7809572f46eee2c0ca143ee
Ibe Ibrahim <ibraheli41@gmail.com>
2026-01-11 19:12:58 +0700

    revision css

--- astro.config.ts
+++ astro.config.ts
@@ -1,45 +1,38 @@
 import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
 
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
 
-import expressiveCode from 'astro-expressive-code'
+import astroExpressiveCode from 'astro-expressive-code'
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
 import rehypeExternalLinks from 'rehype-external-links'
 import rehypeKatex from 'rehype-katex'
-import rehypePrettyCode from 'rehype-pretty-code'
 import remarkEmoji from 'remark-emoji'
 import remarkMath from 'remark-math'
 import rehypeDocument from 'rehype-document'
 
-import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections'
-import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
+// Plugin collapse and line number not working
+// import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections'
+// import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   site: 'https://www.bokumentation.site',
   outDir: './dist',
   integrations: [
-    expressiveCode({
-      themes: ['github-light', 'github-dark'],
-      plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
+    astroExpressiveCode({
+      themes: ['github-light-default', 'dark-plus'],
+      themeCssSelector: (theme) => `[data-theme="${theme.type}"]`,
       useDarkModeMediaQuery: false,
-      themeCssSelector: (theme) => `[data-theme="${theme.name.split('-')[1]}"]`,
       defaultProps: {
         wrap: true,
-        collapseStyle: 'collapsible-auto',
-        overridesByLang: {
-          'ansi,bat,bash,batch,cmd,console,powershell,ps,ps1,psd1,psm1,sh,shell,shellscript,shellsession,text,zsh':
-          {
-            showLineNumbers: false,
-          },
-        },
       },
+
       styleOverrides: {
-        codeFontSize: '0.75rem',
+        codeFontSize: '0.8rem',
         borderColor: 'var(--border)',
         codeFontFamily: 'var(--font-mono)',
         codeBackground:
@@ -60,19 +53,16 @@ export default defineConfig({
           terminalTitlebarBorderBottomColor: 'transparent',
           terminalTitlebarForeground: 'var(--muted-foreground)',
         },
-        lineNumbers: {
-          foreground: 'var(--muted-foreground)',
-        },
-        uiFontFamily: 'var(--font-sans)',
       },
     }),
+
     mdx(),
     react(),
     sitemap(),
     icon(),
   ],
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss() as any],
   },
   server: {
     port: 1234,
@@ -99,15 +89,6 @@ export default defineConfig({
       ],
       rehypeHeadingIds,
       rehypeKatex,
-      [
-        rehypePrettyCode,
-        {
-          theme: {
-            light: 'github-light',
-            dark: 'github-dark',
-          },
-        },
-      ],
     ],
     remarkPlugins: [remarkMath, remarkEmoji],
   },
