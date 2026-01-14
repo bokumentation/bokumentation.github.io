90843ced9462cb660e8c00fe6669ff0b8f342ed4
Ibe Ibrahim <ibraheli41@gmail.com>
2026-01-14 13:54:53 +0700

    revision improve performance

--- astro.config.ts
+++ astro.config.ts
@@ -1,46 +1,51 @@
 import { defineConfig } from 'astro/config'
-import tailwindcss from '@tailwindcss/vite'
 
 import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
 
-import astroExpressiveCode from 'astro-expressive-code'
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
+import expressiveCode from 'astro-expressive-code'
 import rehypeExternalLinks from 'rehype-external-links'
 import rehypeKatex from 'rehype-katex'
+import rehypePrettyCode from 'rehype-pretty-code'
 import remarkEmoji from 'remark-emoji'
 import remarkMath from 'remark-math'
-import rehypeDocument from 'rehype-document'
 
-// Plugin collapse and line number not working
-// import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections'
-// import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
+import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections'
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'https://www.bokumentation.site',
-  outDir: './dist',
+  site: 'https://bokumentation.site',
   integrations: [
-    astroExpressiveCode({
+    expressiveCode({
       themes: ['github-light', 'github-dark'],
-      themeCssSelector: (theme) => `[data-theme="${theme.type}"]`,
+      plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
       useDarkModeMediaQuery: false,
+      themeCssSelector: (theme) => `[data-theme="${theme.name.split('-')[1]}"]`,
       defaultProps: {
         wrap: true,
+        collapseStyle: 'collapsible-auto',
+        overridesByLang: {
+          'ansi,bat,bash,batch,cmd,console,powershell,ps,ps1,psd1,psm1,sh,shell,shellscript,shellsession,text,zsh':
+            {
+              showLineNumbers: false,
+            },
+        },
       },
-
       styleOverrides: {
-        codeFontSize: '0.8rem',
+        codeFontSize: '0.75rem',
         borderColor: 'var(--border)',
         codeFontFamily: 'var(--font-mono)',
         codeBackground:
-          'color-mix(in oklab, var(--secondary) 25%, transparent)',
+          'color-mix(in oklab, var(--muted) 25%, transparent)',
         frames: {
           editorActiveTabForeground: 'var(--muted-foreground)',
           editorActiveTabBackground:
-            'color-mix(in oklab, var(--secondary) 25%, transparent)',
+            'color-mix(in oklab, var(--muted) 25%, transparent)',
           editorActiveTabIndicatorBottomColor: 'transparent',
           editorActiveTabIndicatorTopColor: 'transparent',
           editorTabBorderRadius: '0',
@@ -48,14 +53,17 @@ export default defineConfig({
           editorTabBarBorderBottomColor: 'transparent',
           frameBoxShadowCssValue: 'none',
           terminalBackground:
-            'color-mix(in oklab, var(--secondary) 25%, transparent)',
+            'color-mix(in oklab, var(--muted) 25%, transparent)',
           terminalTitlebarBackground: 'transparent',
           terminalTitlebarBorderBottomColor: 'transparent',
           terminalTitlebarForeground: 'var(--muted-foreground)',
         },
+        lineNumbers: {
+          foreground: 'var(--muted-foreground)',
+        },
+        uiFontFamily: 'var(--font-sans)',
       },
     }),
-
     mdx(),
     react(),
     sitemap(),
@@ -75,12 +83,6 @@ export default defineConfig({
     syntaxHighlight: false,
     rehypePlugins: [
       [
-        rehypeDocument,
-        {
-          css: 'https://cdn.jsdelivr.net/npm/katex@0.16.21/dist/katex.min.css',
-        },
-      ],
-      [
         rehypeExternalLinks,
         {
           target: '_blank',
@@ -89,14 +91,16 @@ export default defineConfig({
       ],
       rehypeHeadingIds,
       rehypeKatex,
+      [
+        rehypePrettyCode,
+        {
+          theme: {
+            light: 'github-light',
+            dark: 'github-dark',
+          },
+        },
+      ],
     ],
     remarkPlugins: [remarkMath, remarkEmoji],
   },
-  i18n: {
-    locales: ["en", "id"],
-    defaultLocale: "en",
-    routing: {
-      prefixDefaultLocale: false
-    }
-  }
 })
